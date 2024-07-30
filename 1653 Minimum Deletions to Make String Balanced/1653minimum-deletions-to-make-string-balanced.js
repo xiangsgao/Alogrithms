ec361211500bf8c862b66b12f4fae44eaa8c01a0
GAO <gaoxiangshuai@gmail.com>
2024-07-30 23:55:53 +0000

    Code challenge submission for question: 1653 Minimum Deletions to Make String Balanced

---
+++ 1653 Minimum Deletions to Make String Balanced/1653minimum-deletions-to-make-string-balanced.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumDeletions = function(s) {
+     var cb = 0, del = 0
+    for(char of s){
+        if(char === 'a')
+            del = Math.min(++del, cb)
+        else
+            cb++
+    }
+    return del
+};
