a4eeb7bf6d9d6508dd5ccf8969f243f700fbd01e
GAO <gaoxiangshuai@gmail.com>
2024-11-10 07:19:20 +0000

    Code challenge submission for question: 97 Interleaving String

---
+++ 97 Interleaving String/97interleaving-string.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {boolean}
+ */
+var isInterleave = function(s1, s2, s3) {
+   let m = s1.length, n = s2.length;
+        if (m + n !== s3.length) {
+            return false;
+        }
+
+        const dp = Array.from({ length: m + 1 }, () =>
+                   Array(n + 1).fill(false));
+        dp[m][n] = true;
+
+        for (let i = m; i >= 0; i--) {
+            for (let j = n; j >= 0; j--) {
+                if (i < m && s1[i] === s3[i + j] && dp[i + 1][j]) {
+                    dp[i][j] = true;
+                }
+                if (j < n && s2[j] === s3[i + j] && dp[i][j + 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[0][0];
+
+};
