fb180ced002536164554e3fea9ccf203d133ce51
GAO <gaoxiangshuai@gmail.com>
2024-11-20 03:45:32 +0000

    Code challenge submission for question: 338 Counting Bits

---
+++ 338 Counting Bits/338counting-bits.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+    const dp = [0];
+    let offset = 1;
+    for(let i = 1; i <= n; i++){
+        if(offset * 2 === i){
+            offset = i;
+        }
+
+        dp[i] = 1 + dp[i - offset];
+    }
+
+    return dp;
+};
