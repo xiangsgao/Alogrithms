74f4ff0a8b3c6d8f0a86ce42196db4125999da62
GAO <gaoxiangshuai@gmail.com>
2025-01-20 17:46:37 -0500

    cleanups

--- 9 Palindrome Number/9palindrome-number.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean isPalindrome(int x) {
-        if(x < 0) return false;
-        String intString = String.valueOf(x);
-        String reverse = new StringBuilder(intString).reverse().toString();
-        return reverse.equals(intString);
-        
-    }
-}
