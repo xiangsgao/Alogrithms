c30269a7b098f6d6a8f6ecdbbe752c0472c30ec4
GAO <gaoxiangshuai@gmail.com>
2025-10-31 20:17:45 +0000

    Code challenge submission for question: 3289 The Two Sneaky Numbers of Digitville

--- 3289 The Two Sneaky Numbers of Digitville/3289the-two-sneaky-numbers-of-digitville.js
+++ 3289 The Two Sneaky Numbers of Digitville/3289the-two-sneaky-numbers-of-digitville.js
@@ -3,14 +3,15 @@
  * @return {number[]}
  */
 var getSneakyNumbers = function(nums) {
-    const map = new Map();
-    const retval = []; 
-    for(const num of nums){
-        map.set(num, (map.get(num) ?? 0) + 1);
-        if(map.get(num) === 2){
-            retval.push(num);
+    const set = new Set();
+    const res = [];
+    for(const n of nums){
+        if(set.has(n)){
+            res.push(n);
+        }
+        set.add(n);
+        if(res.length === 2){
+            return res;
         }
     }
-
-    return retval;
 };
